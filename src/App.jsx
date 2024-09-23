7ab4af7dd22200ca8f9940db4c5d7fabda8a4956
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2024-09-23 16:44:57 +0200

    router vers footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Template from "./components/Template";
+import Footer from "./components/Footer";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <div>
-        <p>Site sport</p>
-      </div>
-    </>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Template />,
+  },
+]);
 
-export default App;
+export default function App() {
+  return <RouterProvider router={router} />;
+}

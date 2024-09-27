9917ed7bfecfd2ada79059e2f1b4f22db2e9c7ec
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2024-09-27 10:25:42 +0200

    bug fix in apx.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from "./components/Header/header"
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header/header";
+import "./App.css";
 
 function App() {
   return (

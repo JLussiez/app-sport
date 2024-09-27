54cee343e00b5d5a6cbe47f1393e5c917ce0461e
4nt0n'1 <antonin.fovelle18@gmail.com>
2024-09-27 15:45:14 +0200

    add gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
   return (
     <>
-      <div>
-        <p>Site sport</p>
-      </div>
+      <Gallery/>
     </>
   );
 }
 
 export default App;
+

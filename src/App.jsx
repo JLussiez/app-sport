bb6bc7edba1fc55f020b7ed7b829091e54230d31
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2024-09-27 13:59:07 +0200

    add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header/header";
+import Contact from "./components/Contact/Contact";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Contact />
     </>
   );
 }
